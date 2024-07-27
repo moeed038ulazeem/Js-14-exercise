c4b023315965a8390751c79f756ee3bb254aaadb
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-07-27 06:31:28 -0700

    ye assighnment 14 ha exercise k sath

---
+++ java.js
@@ -0,0 +1,27 @@
+//problem # 01
+//var numString = "123abc";
+//var priceAfterString = parseInt(numString + 1);
+//console.log(priceAfterString);
+/// the above program output will be 123 because string ma number k baad koi cheez count nahi ho gee
+
+//problem #2
+//var numStr = "45.67xyz";
+//var afterChangingValue = parseFloat(numStr + 1);
+//console.log(afterChangingValue);
+
+//problem 3
+//var string = "123.45";
+//var convertNumber = Number(string);
+//console.log(convertNumber);
+
+
+//problem 4
+//var num = 789;
+//var string = Number.toString(num);
+//console.log(string);
+
+
+//problem 5
+//var str = "256px"
+//var number = parseInt(str);
+//console.log(number);
